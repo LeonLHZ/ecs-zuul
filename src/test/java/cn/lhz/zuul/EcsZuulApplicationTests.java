a6a055fe528694608b967e7bf03120ab8ba463a0
lhz <lhz@zhengmeng.net>
2019-11-18 10:21:35 +0800

    init

---
+++ src/test/java/cn/lhz/zuul/EcsZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lhz.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
